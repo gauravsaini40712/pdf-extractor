87e8c3d27eab85e2a7a5bb91f0578da6ca01a320
gauravsaini40712 <gauravsaini40712@gmail.com>
2026-03-20 18:49:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,4 +25,5 @@ def extract():
 
 if __name__ == "__main__":
     app.run( host="0.0.0.0",   # ✅ IMPORTANT
-        port=int(os.environ.get("PORT", 5000))  # ✅ IMPORTANT)
+        port=int(os.environ.get("PORT", 5000))  # ✅ IMPORTANT
+           )
