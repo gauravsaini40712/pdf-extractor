cbd2c1771a7a0e2b5f3f4b2e0063ec89e334e369
gauravsaini40712 <gauravsaini40712@gmail.com>
2026-03-20 20:11:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,25 +10,8 @@ def home():
 
 @app.route('/extract', methods=['POST'])
 def extract():
-    print("API HIT")
-
-    if 'file' not in request.files:
-        return jsonify({"error": "No file"}), 400
-
-    file = request.files['file']
-    file_path = "temp.pdf"
-    file.save(file_path)
-
-    print("File saved")
-
-    text = extract_text(file_path)
-    print("DEBUG TEXT:", text[:200])  # 👈 add this
-    os.remove(file_path)
-
-    return jsonify({
-        "status": "success",
-        "text": text
-    })
+    return "API WORKING"
+  
 
 if __name__ == "__main__":
     app.run( host="0.0.0.0",   # ✅ IMPORTANT
